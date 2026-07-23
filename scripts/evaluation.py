fcc84c396cb7785f48ffeea2d50b2c133bd38502
Lucas Iturriago <70216214+liturriago@users.noreply.github.com>
2026-07-23 10:58:10 -0500

    Update image zoom and min dist plots in evaluation script

--- scripts/evaluation.py
+++ scripts/evaluation.py
@@ -100,8 +100,8 @@ def run_evaluation(config_path: str, model_path: str, output_dir: str = None) ->
         target_loader=target_loaders['test'], 
         class_names=class_names, 
         prefix="evaluation Target Test - Latent Space",
-        image_zoom=0.07,
-        min_dist_plots=0.45,
+        image_zoom=0.25,
+        min_dist_plots=1.0,
         use_lime=cfg.data.use_lime_on_target,
     )
     
